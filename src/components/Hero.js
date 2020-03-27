6b27d8f68337084f5b4f5d00f3cc22a54989c87b
julio <julio@iMac-de-julio.local>
2020-03-27 16:36:30 -0600

    adding hero

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/hero.css';
+
+import hero_bg from '../images/hero-bg.jpg'
+
+export default class Hero extends Component {
+    render() {
+        return (
+            <div className="hero-wrapper">
+                <div className="background-blend">
+
+                </div>
+            </div>
+        )
+    }
+}
