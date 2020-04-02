d87e97455729506a117bd798b10e7f82135f7e2e
julio <julio@iMac-de-julio.local>
2020-04-01 19:54:44 -0600

    config router

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,12 +3,15 @@ import React from 'react'
 export default function Services() {
     return (
         <div>
-            <div className="services">
+            <div className="serv-inner">
                 <h2 className="t3">Our Services</h2>
+            </div>
+            <div className="serv-cards">
                 <div>
 
                 </div>
             </div>
+
         </div>
     )
 }
